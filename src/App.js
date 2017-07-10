9fc78089f473063cee7e3e8bceda44ce7a4be3ff
plubojacky <plubojacky@email.com>
2017-07-10 13:14:03 -0700

    Finish basic layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    side: 'front'
+  }
+
+  flipCard = () => {
+    const side = this.state.side;
+    let currentSide = '';
+    side === 'front' ? currentSide = 'back' : currentSide = 'front';
+    this.setState({ 
+      side: currentSide 
+    });
+    console.log(this.state.side);
+  };
+
   render() {
     return (
       <div className="App">
-        test
+        <section className="container">
+          <div 
+            className="card"
+            onClick={this.flipCard}
+            >
+            <div className={this.state.side}>
+              {this.state.side} page
+            </div>
+          </div>
+        </section>
       </div>
     );
   }

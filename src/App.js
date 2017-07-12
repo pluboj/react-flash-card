a31cea4deb133ff8c70070a454f6c6196acb6e86
plubojacky <plubojacky@email.com>
2017-07-12 12:12:36 -0700

    Update data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import sch from './img/scheq.png';
 
 const store = [
   {
+      id: 1,
       front:'First Law of Motion',
       back: 'An object at rest will remain at rest unless acted on by an unbalanced force. An object in motion continues in motion with the same speed and in the same direction unless acted upon by an unbalanced force. '
   },
   {
+      id: 2,
       front:'Second Law of Motion',
       back: 'Acceleration is produced when a force acts on a mass. The greater the mass (of the object being accelerated) the greater the amount of force needed (to accelerate the object).'
   },
   {
+      id: 3,
       front:'Third Law of Motion',
       back: 'For every action there is an equal and opposite re-action.'
+  },
+  {
+      id: 4,
+      front:'The Schrödinger equation',
+      back: '',
   }
 ];
 
@@ -45,13 +54,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Flash Cards</h1>
         <section className={this.state.cont}>
           <div 
             className="card"
             onClick={this.flipCard}
             >
             <div className={this.state.side}>
-              <span>{this.state.cards[0][this.state.side]}</span>
+              <span className="id">{this.state.cards[0].id}</span>
+              <span className="question">{this.state.cards[0][this.state.side]}
+                {this.state.cards[0][this.state.side] === '' ? <img src={sch} alt='pic'/> : '' }
+              </span>              
             </div>
           </div>
         </section>

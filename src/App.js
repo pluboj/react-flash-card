2d66037f112a8ae52ad2df93753d346f6bc37aa7
plubojacky <plubojacky@email.com>
2017-07-12 13:39:30 -0700

    Add data.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import sch from './img/scheq.png';
-
-const store = [
-  {
-      id: 1,
-      front:'First Law of Motion',
-      back: 'An object at rest will remain at rest unless acted on by an unbalanced force. An object in motion continues in motion with the same speed and in the same direction unless acted upon by an unbalanced force. '
-  },
-  {
-      id: 2,
-      front:'Second Law of Motion',
-      back: 'Acceleration is produced when a force acts on a mass. The greater the mass (of the object being accelerated) the greater the amount of force needed (to accelerate the object).'
-  },
-  {
-      id: 3,
-      front:'Third Law of Motion',
-      back: 'For every action there is an equal and opposite re-action.'
-  },
-  {
-      id: 4,
-      front:'The Schrödinger equation',
-      back: <img src={sch} alt='pic'/>,
-  }
-];
+import { store } from './data.js';
 
 class App extends Component {
 
   state = {
     side: 'front',
     cont: 'container',
-    cards: store
+    cards: store()
   }
 
   flipCard = () => {
